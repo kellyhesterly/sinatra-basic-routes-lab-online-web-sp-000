4d7f836782dd340099e42a76fffb60d5b412e7de
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-20 15:39:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/name' do
     "My name is Kelly"
-  end
 end
 
   get '/hometown' do
     "My hometown is Alpine"
-  end
 end
 
 get '/favorite-song' do
   "My favorite song is Baby Shark"
-  end
+end
+
 end
